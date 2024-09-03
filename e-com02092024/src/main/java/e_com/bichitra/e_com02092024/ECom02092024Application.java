05f6de0dd68b9a194dbd104888ab0c96b0ac27fc
bichitra <bichitramohansngha@gmail.com>
2024-09-03 11:59:00 +0530

    first commit

---
+++ e-com02092024/src/main/java/e_com/bichitra/e_com02092024/ECom02092024Application.java
@@ -0,0 +1,13 @@
+package e_com.bichitra.e_com02092024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECom02092024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECom02092024Application.class, args);
+	}
+
+}
---
+++ e-com02092024/src/test/java/e_com/bichitra/e_com02092024/ECom02092024ApplicationTests.java
@@ -0,0 +1,13 @@
+package e_com.bichitra.e_com02092024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECom02092024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
